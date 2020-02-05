7efeb73cf95cf8f21c1189187845beccc76c8f3c
Adane <Adane1301992@gmail.com>
2020-02-05 14:21:54 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne ="queen";
+let carTwo="queen";
+let cardThree="king";
+let cardfour="king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
