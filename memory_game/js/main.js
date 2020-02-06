8032b374d78028d45bee536d900cfc126e01dbc2
Adane <Adane1301992@gmail.com>
2020-02-05 18:34:45 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and running!");
-let cardOne ="queen";
-let carTwo="queen";
-let cardThree="king";
-let cardfour="king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne); 
+console.log("user flipped"+" "+cardOne);
+let cardTwo =cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped"+" "+cardTwo);
+
+ if (cardsInPlay[0]===cardsInPlay[1]){
+	alert("You found a match!");
+}
+
+else {
+	alert("sory try againg");
+}
+	
