de29c5f9f1c4a9a18c19c306ee445337ff7c53dd
Adane <Adane1301992@gmail.com>
2020-02-08 13:01:12 -0600

    created logic game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,23 @@
-
-	 const cards = ["queen", "queen", "king", "king"];
+const cards = [ 
+       { rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+        },
+        {
+      rank: "queen",
+      suit: "diamends",
+      cardImage: "images/queen-of-diamonds.png"
+  }, 
+      {
+       rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+      }, 
+      {
+        rank: "kign",
+        suit: "diamends",
+        cardImage: "images/king-of-diamonds.png"
+      }];
 const cardsInPlay = [];
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,9 +27,13 @@ function checkForMatch() {
 }
  }
 function flipcard(cardId){
-  console.log("User flipped"+" "+cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-  }
+  console.log("User flipped"+" "+cards[cardId].rank );
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].image);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].image);
+  cardsInPlay.push(cards[cardId].suit);
+  checkForMatch();
+}
 flipcard(0);
 flipcard(2);
-checkForMatch()
